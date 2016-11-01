7c36398d5bd15e4b0cd6d28bf27012943632be06
sfindak2 <sfindak2@.mail.lakelandcc.edu>
2016-11-01 07:59:04 -0400

    Generated on work computer in BandC_R2

--- src/srcFile.py
+++ src/srcFile.py
@@ -1,2 +1,10 @@
-print "Hello everone this is a file for the branch and clone excersise"
-raw_input("Hit Enter to continue: ")
+from workComputer import hiFromWork
+
+def helloFunc():
+	print "Hello everone this is a file for the branch and clone excersise"
+	raw_input("Hit Enter to continue: ")
+	hiFromWork()
+	
+
+helloFunc()
+
---
+++ src/workComputer.py
@@ -0,0 +1,6 @@
+
+def hiFromWork():
+	print ('This function hiFromWork() was generated on my work computer')
+	raw_input("Hit Enter to continue: ")
+	
+
